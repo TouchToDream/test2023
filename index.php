f916c10865f757f2fccd64590c2ae49ceb80bf85
TouchToDream <s1lence10@hotmail.com>
2023-03-13 13:01:06 +0200

    update array

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
-$goods = [];
-$goods[0] = ['price' => 100, 'date' => date('Y-m-d'), 'pic' => 'pics/service.jpg', 'service' => 'Ремонт телефонов'];
-$goods[1] = ['price' => 75, 'date' => date('Y-m-d'), 'pic' => 'pics/service.jpg', 'service' => 'Ремонт компьютеров'];
-$goods[2] = ['price' => 25, 'date' => date('Y-m-d'), 'pic' => 'pics/service.jpg', 'service' => 'Ремонт ноутбуков'];
-$goods[3] = ['price' => 35, 'date' => date('Y-m-d'), 'pic' => 'pics/service.jpg', 'service' => 'Ремонт телевизоров'];
-$goods[4] = ['price' => 65, 'date' => date('Y-m-d'), 'pic' => 'pics/service.jpg', 'service' => 'Ремонт планшетов'];
-$goods[5] = ['price' => 99, 'date' => date('Y-m-d'), 'pic' => 'pics/service.jpg', 'service' => 'Продажа комплектующих'];
+$goods = [
+	['price' => 100, 'date' => date('Y-m-d'), 'pic' => 'pics/service.jpg', 'service' => 'Ремонт телефонов'],
+	['price' => 75, 'date' => date('Y-m-d'), 'pic' => 'pics/service.jpg', 'service' => 'Ремонт компьютеров'],
+	['price' => 25, 'date' => date('Y-m-d'), 'pic' => 'pics/service.jpg', 'service' => 'Ремонт ноутбуков'],
+	['price' => 35, 'date' => date('Y-m-d'), 'pic' => 'pics/service.jpg', 'service' => 'Ремонт телевизоров'], 
+	['price' => 65, 'date' => date('Y-m-d'), 'pic' => 'pics/service.jpg', 'service' => 'Ремонт планшетов'], 
+	['price' => 99, 'date' => date('Y-m-d'), 'pic' => 'pics/service.jpg', 'service' => 'Продажа комплектующих']
+];
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -62,40 +62,40 @@ $goods[5] = ['price' => 99, 'date' => date('Y-m-d'), 'pic' => 'pics/service.jpg'
 	<div class="container">
 		<h1 class="text-center">Услуги</h1>
 
-<?php foreach($goods as $item): ?>		
-		<div class="col-sm-3">
-			<table class="table table-bordered">
-				<tr>
-					<td class="text-center" colspan="2">
-						<img src="<?php echo $item['pic']; ?>" class="img-thumbnail center">
-					</td>
-				</tr>
-				<tr>
-					<td class="text-center" colspan="2">
-						<?php echo $item['service']; ?>
-					</td>
-				</tr>
-				<tr>
-					<td class="text-center">
-						<?php echo $item['price']; ?>
-					</td>
-					<td class="text-center">
-						<?php echo $item['date']; ?>
-					</td>
-				</tr>
-			</table>
-		</div>
-<?php endforeach; ?>
+		<?php foreach ($goods as $item) : ?>
+			<div class="col-sm-3">
+				<table class="table table-bordered">
+					<tr>
+						<td class="text-center" colspan="2">
+							<img src="<?php echo $item['pic']; ?>" class="img-thumbnail center">
+						</td>
+					</tr>
+					<tr>
+						<td class="text-center" colspan="2">
+							<?php echo $item['service']; ?>
+						</td>
+					</tr>
+					<tr>
+						<td class="text-center">
+							<?php echo $item['price']; ?>
+						</td>
+						<td class="text-center">
+							<?php echo $item['date']; ?>
+						</td>
+					</tr>
+				</table>
+			</div>
+		<?php endforeach; ?>
 
 
-	<!-- Bootstrap core JavaScript
+		<!-- Bootstrap core JavaScript
 		================================================== -->
-	<!-- Placed at the end of the document so the pages load faster -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
